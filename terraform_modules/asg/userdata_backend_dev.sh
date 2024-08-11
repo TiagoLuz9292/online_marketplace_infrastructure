428a512f360f4020580fd952f54bace544d810da
TiagoLuz9292 <tiagoluz92@gmail.com>
2024-08-11 21:03:00 +0100

    updates

--- terraform_modules/asg/userdata_backend_dev.sh
+++ terraform_modules/asg/userdata_backend_dev.sh
@@ -12,8 +12,8 @@ export APP_VERSION="0.0.3"  # Example version, change this accordingly
 
 # Pull the latest code from the repository
 cd /home/ec2-user
-git clone https://github.com/TiagoLuz9292/online_marketplace.git
-cd online_marketplace
+https://github.com/TiagoLuz9292/online_marketplace_backend.git
+cd online_marketplace_backend
 
 # Install Node.js
 curl -sL https://rpm.nodesource.com/setup_18.x | bash -
@@ -23,15 +23,11 @@ yum install -y nodejs
 npm install pm2@latest -g
 
 # Install application dependencies
-cd backend
 npm install
 
-cd ../frontend
-npm install
 
 # Start the backend and frontend applications using PM2
 pm2 start /home/ec2-user/online_marketplace/backend/app.js --name backend --log /var/log/backend.log
-pm2 start /home/ec2-user/online_marketplace/frontend/src/app.js --name frontend --log /var/log/frontend.log
 
 # Save the PM2 process list and enable PM2 startup on reboot
 pm2 save
